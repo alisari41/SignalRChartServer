923777b85b570bdf51266ef579f3bb2f9bc7f287
alisari41 <alisari41@outlook.com>
2022-02-19 23:46:55 +0300

    Güncelleme

--- SignalRChartServer/Hubs/SatisHub.cs
+++ SignalRChartServer/Hubs/SatisHub.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
 
 namespace SignalRChartServer.Hubs
 {
-    public class SatisHub
+    public class SatisHub : Hub
     {
+
     }
 }
